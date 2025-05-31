268024f14c013af99a8cf54d66646d45111e149b
nicenicenice <112688217+nashie1004@users.noreply.github.com>
2025-05-31 15:10:03 +0800

    Add project files.

---
+++ ci-cd-testing.library/Calculator.cs
@@ -0,0 +1,30 @@
+﻿namespace ci_cd_testing.library
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ ci-cd-testing.tests/CalculatorTest.cs
@@ -0,0 +1,43 @@
+﻿using ci_cd_testing.library;
+
+namespace ci_cd_testing.tests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void Three_Add_Two_Equals_Three()
+        {
+            var calculator = new Calculator();
+            int sum = calculator.Add(3, 2);
+
+            Assert.Equal(5, sum);
+        }
+
+        [Fact]
+        public void Three_Minus_Two_Equals_One()
+        {
+            var calculator = new Calculator();
+            int difference = calculator.Subtract(3, 2);
+
+            Assert.Equal(1, difference);
+        }
+
+        [Fact]
+        public void Three_Multiply_Two_Equals_Six()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Multiply(3, 2);
+
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void Three_Divide_Three_Equals_One()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Divide(3, 3);
+
+            Assert.Equal(1, result);
+        }
+    }
+}
